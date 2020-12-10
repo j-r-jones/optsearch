9cd9ea93dffd1312d2558c34bfe8077896005832
J R Jones <j-r-jones@users.noreply.github.com>
2020-12-10 15:38:57 +0000

    Added some verbose comments and changed the way that the values returned from the database are printed.  This is not as robust as it should be.  It will fail if something went wrong and the database is empty.

--- scripts/checkdb.py
+++ scripts/checkdb.py
@@ -50,22 +50,25 @@ def main(args):
         try:
             cur = conn.cursor()
 
+            # Warning: If the database is empty, these will fail.
+            # That should only happen if something went really wrong
+
             print('\nNumber of positions searched: ',)
             cur.execute('select count(id) from position;')
-            print(cur.fetchone())
+            print(cur.fetchone()[0])
 
             print('\nTotal number of evaluations (all visits) is: ',)
             cur.execute('SELECT SUM(visits) FROM position;')
-            print(cur.fetchone())
+            print(cur.fetchone()[0])
 
         
             print("\nMaximum number of visits to any single position is: ",)
             cur.execute("SELECT MAX(visits) FROM position;")
-            print(cur.fetchone())
+            print(cur.fetchone()[0])
 
             print("\nAverage number of visits per position is: ",)
             cur.execute("SELECT AVG(visits) FROM position;")
-            print(cur.fetchone())
+            print(cur.fetchone()[0])
 
             # This one doesn't produce anything useful
             #print("Top 10 positions by fitness (fitness,visits,id):")
@@ -74,10 +77,11 @@ def main(args):
             #sorted_rows = sorted(rows, key=lambda fitness: fitness[0])
 
             print("\nTop 10 most visited positions:")
-            cur.execute("SELECT visits,fitness,id FROM position;")
+            cur.execute("SELECT id,fitness,visits FROM position;")
             rows = cur.fetchall()
-            sorted_rows = sorted(rows, key=lambda visits: visits[0])
+            sorted_rows = sorted(rows, key=lambda visits: visits[2])
             count = 0
+            print('Id, Fitness, Visits:')
             for row in sorted_rows:
                 count = count + 1
                 print(row)
@@ -85,13 +89,23 @@ def main(args):
                     break
                 
             print('\nAll best positions found:')
+            print('Timestamp, PositionID:')
             for row in cur.execute('select * from global_best_history;'):
                 print(row)
+
+            # If all our fitnesses are UINT_MAX, then most likely every
+            # attempt to assess fitness failed.  That means that something
+            # failed at all positions visited.  Compiling may have failed,
+            # execution of a wrapper script, benchmark or test may have failed,
+            # etc.  OptSearch expects quite a few failures to be encountered,
+            # but is likely to think the search converged if we never "move"
+            # (ie fitness does not change).
 
             # Did the fitnesses of the positions change much?
             cur.execute('select fitness from position;')
             rows = cur.fetchall()  # This gives us a list of tuples
-            print('\nFitnesses found vary between minimum: ', min(rows), ' and maximum: ', max(rows), '\n')
+            print('\nFitnesses found vary between minimum: ', min(rows)[0], ' and maximum: ', max(rows)[0], '\n')
+
         except sqlite3.Error as e:
             print("An error was encountered trying to query the database:", e.args[0])
 
